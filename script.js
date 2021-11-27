b7f0b1552c837cecd4b6bee3e5809cdee7ddd098
SkanderJmaiel <jemaielskander@gmail.com>
2021-11-27 14:20:03 +0000

    ok

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function compute()
+
+function compute(event)
+{   
+
+
+    var p = document.getElementById("principal").value;
+    if(p<=0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    else{
+
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = p*years*rate/100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var result = document.getElementById("result");
+    result.innerHTML= "If you deposit "+p+"<br/> at and interest rate of "+rate+". <br/> You will receive an amount of "+interest+",<br/> in the year"+year;
+    }
+
+}
+function updateRate() 
 {
-    p = document.getElementById("principal").value;
-    
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
